1f7ddae6bae8c9f3513022c1c69e7e80f889e0ef
oguzsarikaya <151478834+oguzsarikaya@users.noreply.github.com>
2023-11-22 16:40:24 +0300

    cursor-changer.js

--- cursor-changer.js
+++ cursor-changer.js
@@ -1,13 +1,22 @@
-document.addEventListener('DOMContentLoaded', function() {
-  document.body.style.cursor = 'url("https://static.ticimax.cloud/62286//uploads/editoruploads/mause-cursor-32.png"), auto';
-  var clickableItems = document.querySelectorAll('a, button, input[type="submit"], input[type="button"]');
-  clickableItems.forEach(function(item) {
-    item.addEventListener('mouseover', function() {
-      item.style.cursor = 'url("https://static.ticimax.cloud/62286//uploads/editoruploads/mause-cursor-50-180.png"), auto';
-    });
+<script>
+  document.addEventListener('DOMContentLoaded', function() {
+    // Varsayılan imleç
+    document.body.style.cursor = 'url("https://static.ticimax.cloud/62286//uploads/editoruploads/mause-cursor-32.png"), auto';
 
-    item.addEventListener('mouseout', function() {
-      item.style.cursor = 'url("https://static.ticimax.cloud/62286//uploads/editoruploads/mause-cursor-32.png"), auto';
+    // Tıklanabilir öğeler için imleç
+    var clickableItems = document.querySelectorAll('a, button, input[type="submit"], input[type="button"]');
+
+    // Tıklanabilir öğelerin üzerine gelindiğinde ve ayrıldığında imleci değiştir
+    clickableItems.forEach(function(item) {
+      item.addEventListener('mouseover', function() {
+        // Mouse üzerine gelince döndürülmüş imleci kullan
+        item.style.cursor = 'url("https://static.ticimax.cloud/62286//uploads/editoruploads/mause-cursor-50-180.png"), auto';
+      });
+
+      item.addEventListener('mouseout', function() {
+        // Mouse ayrılınca varsayılan imlece dön
+        item.style.cursor = 'url("https://static.ticimax.cloud/62286//uploads/editoruploads/mause-cursor-32.png"), auto';
+      });
     });
   });
-});
+</script>
